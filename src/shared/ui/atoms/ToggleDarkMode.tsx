d8f9415d8c7557ab6c5c85cd052a65b91dc14b3b
Rafael Moro G <rafael.moro.galindo@gmail.com>
2025-06-05 13:51:16 -0600

    add logic to change state of color theme

--- src/shared/ui/atoms/ToggleDarkMode.tsx
+++ src/shared/ui/atoms/ToggleDarkMode.tsx
@@ -1,10 +1,25 @@
 "use client"
+import { useState } from "react";
 import { Button } from "flowbite-react";
 import { NightIcon } from "@/shared/ui/icons/Nights";
+import { ThemeMode } from "@/shared/constants/Global.constants";
 
 export function ToggleDarkMode() {
+  const [mode, setMode] = useState<ThemeMode>('dark');
+  const toggleDarkMode = () => {
+    const htmlElement = document.documentElement;
+
+    if (mode === 'light') {
+      setMode('dark')
+      htmlElement.setAttribute("data-theme", "dark");
+      return
+    }
+    setMode('light')
+    htmlElement.setAttribute("data-theme", "light");
+  }
+
 return (
-  <Button color="dark" outline>
+  <Button onClick={toggleDarkMode} color="dark" outline>
     <NightIcon />
   </Button>
 )
