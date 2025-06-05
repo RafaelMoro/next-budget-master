b7d2665c997189d3163b71fad60eea6cec3f12ef
Rafael Moro G <rafael.moro.galindo@gmail.com>
2025-06-05 13:34:43 -0600

    show header in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import { Header } from "@/shared/ui/organisms/Header";
+
 export default function Home() {
   return (
     <div>
+      <Header />
       <h1 className="text-black dark:text-white">Home</h1>
     </div>
   );
