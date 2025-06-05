cc74566b7baa2e292fc8eb75666d6e8edfa6c827
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-05 10:23:02 -0600

    add tremor area chart test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { AreaChartTypeExample } from "@/components/AreaChartTest";
+
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <AreaChartTypeExample />
     </div>
   );
 }
