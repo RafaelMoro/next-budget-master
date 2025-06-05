4b3b16c0413315858dfd9aee2db41483fbed6acc
Rafael Moro G <rafael.moro.galindo@gmail.com>
2025-06-05 14:36:06 -0600

    add login card into main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import { LoginCard } from "@/features/Login/LoginCard";
 import { Header } from "@/shared/ui/organisms/Header";
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <h1 className="text-black dark:text-white">Home</h1>
+      <main className="flex flex-col justify-center items-center gap-5">
+        <h1 className="text-black dark:text-white text-4xl text-center font-bold">Bienvenido de vuelta</h1>
+        <LoginCard />
+      </main>
     </div>
   );
 }
