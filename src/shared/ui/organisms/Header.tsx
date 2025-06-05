dfedf40ec772384f001a7da0556ad100cbca0e0b
Rafael Moro G <rafael.moro.galindo@gmail.com>
2025-06-05 13:59:20 -0600

    align header

--- src/shared/ui/organisms/Header.tsx
+++ src/shared/ui/organisms/Header.tsx
@@ -3,7 +3,7 @@ import { ToggleDarkMode } from "../atoms/ToggleDarkMode"
 
 export const Header = () => {
   return (
-    <header className="p-4 flex flex-row justify-between">
+    <header className="p-4 flex flex-row justify-between items-center">
       <Image className="rounded-full" src="/img/logo-no-bg.webp" alt="Budget Master Logo" width={70} height={70} />
       <ToggleDarkMode />
     </header>
