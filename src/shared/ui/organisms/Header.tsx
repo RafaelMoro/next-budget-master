f4cb0855c7895e1c170f9f657ae8bcc53995c7c8
Rafael Moro G <rafael.moro.galindo@gmail.com>
2025-06-05 13:34:31 -0600

    add header organism

---
+++ src/shared/ui/organisms/Header.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image"
+import { ToggleDarkMode } from "../atoms/ToggleDarkMode"
+
+export const Header = () => {
+  return (
+    <header className="p-4 flex flex-row justify-between">
+      <Image className="rounded-full" src="/img/logo-no-bg.webp" alt="Budget Master Logo" width={70} height={70} />
+      <ToggleDarkMode />
+    </header>
+  )
+}
