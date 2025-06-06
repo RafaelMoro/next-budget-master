fecc7d92ede17053bd6620ebddfcdc1cd018ec1a
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-05 23:42:01 -0600

    test: add AppRouterContextProviderMock for router testing

--- __tests__/home.test.tsx
+++ __tests__/home.test.tsx
@@ -1,12 +1,18 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import Home from '../src/app/page'
+import { AppRouterContextProviderMock } from '@/shared/ui/organisms/AppRouterContextProviderMock';
  
 describe('Home', () => {
   it('renders a heading', () => {
-    render(<Home />)
+    const push = jest.fn();
+    render(
+      <AppRouterContextProviderMock router={{ push }}>
+        <Home />
+      </AppRouterContextProviderMock>
+  )
  
-    const heading = screen.getByRole('heading', { name: /home/i })
+    const heading = screen.getByRole('heading', { name: /bienvenido de vuelta/i })
  
     expect(heading).toBeInTheDocument()
   })
