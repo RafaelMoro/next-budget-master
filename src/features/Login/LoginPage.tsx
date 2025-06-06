ca6bb99244518dbb17baf357dc3c994dd5303522
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-05 23:11:00 -0600

    refactor: increase gap between elements in LoginPage layout

--- src/features/Login/LoginPage.tsx
+++ src/features/Login/LoginPage.tsx
@@ -7,7 +7,7 @@ const queryClient = new QueryClient()
 export const LoginPage = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <main className="flex-1 flex flex-col justify-center items-center gap-5 min-h-full">
+      <main className="flex-1 flex flex-col justify-center items-center gap-20 min-h-full">
         <h1 className="text-black dark:text-white text-4xl text-center font-bold">Bienvenido de vuelta</h1>
         <LoginCard />
       </main>
