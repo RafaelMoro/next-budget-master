39ca34b3218ced045b513a2925823036a1157b98
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-05 23:07:33 -0600

    refactor: restructure layout and remove redundant Header component from LoginPage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Header } from "@/shared/ui/organisms/Header";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white dark:bg-black transition-colors`}
       >
-        {children}
+        <div className="min-h-screen flex flex-col">
+          <Header />
+          {children}
+        </div>
       </body>
     </html>
   );
--- src/features/Login/LoginPage.tsx
+++ src/features/Login/LoginPage.tsx
@@ -1,5 +1,4 @@
 "use client"
-import { Header } from "@/shared/ui/organisms/Header";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { LoginCard } from "./LoginCard";
 
@@ -8,13 +7,10 @@ const queryClient = new QueryClient()
 export const LoginPage = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <div>
-        <Header />
-        <main className="flex flex-col justify-center items-center gap-5">
-          <h1 className="text-black dark:text-white text-4xl text-center font-bold">Bienvenido de vuelta</h1>
-          <LoginCard />
-        </main>
-      </div>
+      <main className="flex-1 flex flex-col justify-center items-center gap-5 min-h-full">
+        <h1 className="text-black dark:text-white text-4xl text-center font-bold">Bienvenido de vuelta</h1>
+        <LoginCard />
+      </main>
     </QueryClientProvider>
   )
 }
