ef533f44a40493193a73e7796e1f9782d11613f4
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-06 01:15:39 -0600

    change dashboard fn for default

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-export function DashboardPage () {
+export default function DashboardPage () {
   return (
     <h1 className="text-black dark:text-white text-4xl text-center font-bold">Dashboard</h1>
   )
