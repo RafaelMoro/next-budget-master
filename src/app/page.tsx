58a60183b4545dc4fd6ddc7fa611aa9e1ce4b2ee
Rafael Moro G <rafael.moro.galindo@gmail.com>
2025-06-05 19:40:38 -0600

    add tanstack query config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,23 @@
+"use client"
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 import { LoginCard } from "@/features/Login/LoginCard";
 import { Header } from "@/shared/ui/organisms/Header";
 
+const queryClient = new QueryClient()
+
 export default function Home() {
   return (
-    <div>
-      <Header />
-      <main className="flex flex-col justify-center items-center gap-5">
-        <h1 className="text-black dark:text-white text-4xl text-center font-bold">Bienvenido de vuelta</h1>
-        <LoginCard />
-      </main>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <Header />
+        <main className="flex flex-col justify-center items-center gap-5">
+          <h1 className="text-black dark:text-white text-4xl text-center font-bold">Bienvenido de vuelta</h1>
+          <LoginCard />
+        </main>
+      </div>
+    </QueryClientProvider>
   );
 }
