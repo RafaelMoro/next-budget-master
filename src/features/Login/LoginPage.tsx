e332134a6dba662c928911fd9cc8ccbe3e7e9533
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-05 19:49:42 -0600

    add login page client

---
+++ src/features/Login/LoginPage.tsx
@@ -0,0 +1,20 @@
+"use client"
+import { Header } from "@/shared/ui/organisms/Header";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { LoginCard } from "./LoginCard";
+
+const queryClient = new QueryClient()
+
+export function LoginPage() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <Header />
+        <main className="flex flex-col justify-center items-center gap-5">
+          <h1 className="text-black dark:text-white text-4xl text-center font-bold">Bienvenido de vuelta</h1>
+          <LoginCard />
+        </main>
+      </div>
+    </QueryClientProvider>
+  )
+}
