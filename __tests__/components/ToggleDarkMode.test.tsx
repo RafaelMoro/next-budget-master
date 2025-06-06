08c33adc888cdd43c1c11aa6b9200a93b78d29e1
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-06 12:41:28 -0600

    test: add interaction test for ToggleDarkMode component

--- __tests__/components/ToggleDarkMode.test.tsx
+++ __tests__/components/ToggleDarkMode.test.tsx
@@ -1,10 +1,20 @@
 import { ToggleDarkMode } from '@/shared/ui/atoms/ToggleDarkMode'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 describe('ToggleDarkMode', () => {
   it('Should show toggle dark mode button', () => {
     render(<ToggleDarkMode />)
 
     expect(screen.getByTestId('toggle-theme-mode-button')).toBeInTheDocument()
+  })
+
+  it('Should change to light mode when clicked', async() => {
+    const user = userEvent.setup()
+    render(<ToggleDarkMode />)
+
+    const button = screen.getByTestId('toggle-theme-mode-button')
+    await user.click(button)
+    expect(document.documentElement.getAttribute('data-theme')).toBe('light')
   })
 })
