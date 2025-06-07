776f4c449b2fbf6715c61d2b0858605eaab3612a
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-07 01:19:36 -0600

    feat: refactor DashboardPage to use ShowAccounts component for account display

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import { GetAccountResponse } from '@/shared/types/Accounts.types'
-import { Card } from "flowbite-react";
+
 import { getAccessToken } from '@/shared/lib/auth';
+import { ShowAccounts } from '@/features/Accounts/ShowAccounts';
 
 export default async function DashboardPage () {
   const accessToken = await getAccessToken()
@@ -15,16 +16,7 @@ export default async function DashboardPage () {
   return (
     <div className='grid grid-col-3 gap-4 justify-items-center'>
       <h1 className="text-black dark:text-white text-4xl text-center font-bold col-span-3">Accounts</h1>
-      { accounts.map((account) => (
-        <Card key={account._id} href="#" className="max-w-sm">
-          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            {account.title}
-          </h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            {account.amount}
-          </p>
-        </Card>
-      )) }
+      <ShowAccounts accounts={accounts} />
     </div>
   )
 }
