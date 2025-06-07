a64b5e6b778b683bce687c5496e6171dac9e277e
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-06 22:46:27 -0600

    feat: update POST handler to set accessToken cookie and log it in DashboardPage

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,18 +1,20 @@
 import axios from "axios";
 import { type NextRequest } from 'next/server'
 import { cookies } from 'next/headers'
-import { parseSetCookieString } from "@/shared/utils/parseCookie";
 
 export async function POST(request: NextRequest) {
   const payload = await request.json()
   const uri = `${process.env.NEXT_PUBLIC_BACKEND_URI}/auth/`
   const res = await axios.post(uri, payload)
   const cookiesReceived = res.headers['set-cookie']
+
   if (cookiesReceived) {
     const [cookie] = cookiesReceived
-    console.log('cookie', cookie)
-    const cookieTransformed = parseSetCookieString(cookie)
-    cookies().set(cookieTransformed.name, cookieTransformed.value, cookieTransformed.options)
+    cookies().set('accessToken', cookie, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+    })
   }
   return new Response(JSON.stringify(res.data), {
     status: 201,
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,14 @@
+import { cookies } from 'next/headers'
 export default async function DashboardPage () {
+  const cookie = cookies().get('accessToken')
+  console.log('cookie', cookie)
+  const hardcodedCookie = 'accessToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NTZjZTJhYmZlMzgwNjg0NjY1ZTkyYTMiLCJpYXQiOjE3NDkyNjE3NTgsImV4cCI6MTc0OTY5Mzc1OH0.zd73FvC_HxLA6XpPZ8vRG96x4p1gTixB3OM3MJQy73U; Max-Age=432000; Path=/; Expires=Thu, 12 Jun 2025 02:02:38 GMT; HttpOnly'
+
   const data = await fetch('http://localhost:6006/account-actions', {
     credentials: 'include',
     headers: {
       'Content-Type': 'application/json',
-      'Cookie': 'accessToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NTZjZTJhYmZlMzgwNjg0NjY1ZTkyYTMiLCJpYXQiOjE3NDkyNjE3NTgsImV4cCI6MTc0OTY5Mzc1OH0.zd73FvC_HxLA6XpPZ8vRG96x4p1gTixB3OM3MJQy73U; Max-Age=432000; Path=/; Expires=Thu, 12 Jun 2025 02:02:38 GMT; HttpOnly'
+      'Cookie': hardcodedCookie
     }
   })
   console.log('data', data)
