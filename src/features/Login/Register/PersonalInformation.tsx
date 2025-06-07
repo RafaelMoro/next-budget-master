817122453fde2592abc233b20df4bb579f835d1f
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-07 12:54:33 -0600

    test ract hook forms in personal information

--- src/features/Login/Register/PersonalInformation.tsx
+++ src/features/Login/Register/PersonalInformation.tsx
@@ -1,23 +1,29 @@
 "use client"
-import { FormEvent, useState } from "react";
-import { Card, Button, Label, TextInput } from "flowbite-react";
 import Link from "next/link";
+import { Card, Button, Label, TextInput } from "flowbite-react";
+import { useForm, SubmitHandler } from "react-hook-form"
 
 interface PersonalInformationProps {
   nextCb: () => void;
 }
 
-export const PersonalInformation = ({ nextCb }: PersonalInformationProps) => {
-  const [firstName, setfirstName] = useState("");
-  const [middleName, setMiddleName] = useState("");
-  const [lastName, setLastName] = useState("");
+type Inputs = {
+  firstName: string
+  middleName: string
+  lastName: string
+}
 
-  const handleChangeFirstName = (event: React.ChangeEvent<HTMLInputElement>) => setfirstName(event.target.value);
-  const handleChangeMiddletName = (event: React.ChangeEvent<HTMLInputElement>) => setMiddleName(event.target.value);
-  const handleChangeLasttName = (event: React.ChangeEvent<HTMLInputElement>) => setLastName(event.target.value);
-  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
+export const PersonalInformation = ({ nextCb }: PersonalInformationProps) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>()
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data)
+    nextCb()
   }
+  console.log('errors', errors)
 
   return (
     <Card className="max-w-sm">
@@ -25,27 +31,27 @@ export const PersonalInformation = ({ nextCb }: PersonalInformationProps) => {
         Crear cuenta.
       </h5>
       <p className="text-xl text-black dark:text-white">Llene la siguiente información para crear su cuenta.</p>
-      <form onSubmit={(event) => handleSubmit(event)} className="flex max-w-md flex-col gap-4">
+      <form onSubmit={handleSubmit(onSubmit)} className="flex max-w-md flex-col gap-4">
         <div>
           <div className="mb-2 block">
             <Label htmlFor="firstName">Primer Nombre</Label>
           </div>
-          <TextInput value={firstName} id="firstName" type="text" onChange={handleChangeFirstName} />
+          <TextInput id="firstName" type="text" {...register("firstName", { required: true, minLength: 2 })} />
         </div>
         <div>
           <div className="mb-2 block">
             <Label htmlFor="middleName">Segundo Nombre (Opcional)</Label>
           </div>
-          <TextInput value={middleName} id="middleName" type="text" onChange={handleChangeMiddletName} />
+          <TextInput id="middleName" type="text" {...register("middleName", { minLength: 2 })} />
         </div>
         <div>
           <div className="mb-2 block">
-            <Label htmlFor="middleName">Apellido</Label>
+            <Label htmlFor="lastName">Apellido</Label>
           </div>
-          <TextInput value={lastName} onChange={handleChangeLasttName} id="middleName" type="text" />
+          <TextInput id="lastName" type="text" {...register("lastName", { required: true, minLength: 2 })} />
         </div>
         <Link className="relative flex items-center justify-center rounded-lg text-center font-medium focus:outline-none focus:ring-4 h-10 px-5 text-sm border border-primary-700 text-primary-700 hover:border-primary-800 hover:bg-primary-800 hover:text-white focus:ring-primary-300 dark:border-primary-600 dark:text-primary-500 dark:hover:border-primary-700 dark:hover:bg-primary-700 dark:hover:text-white dark:focus:ring-primary-800" href="/">Volver</Link>
-        <Button onClick={nextCb} className="hover:cursor-pointer">
+        <Button type="submit" className="hover:cursor-pointer">
           Siguiente
         </Button>
       </form>
