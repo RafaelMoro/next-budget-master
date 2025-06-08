0098cea409ce7530e2d08cc51f85ef44e1340698
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-07 18:24:00 -0600

    add messaging

--- src/shared/constants/Global.constants.ts
+++ src/shared/constants/Global.constants.ts
@@ -4,3 +4,9 @@ export const COOKIE_SESSION_KEY = 'session'
 // Routes
 export const DASHBOARD_ROUTE = '/dashboard';
 export const LOGIN_ROUTE = '/';
+
+// Messaging
+export const SUCCESS_CREATE_USER_TITLE = 'Usuario creado con éxito';
+export const SUCCESS_CREATE_USER_MESSAGE = 'Inicie sesión para continuar';
+export const ERROR_CREATE_USER_TITLE = 'Oops! Algo no salió como esperabamos.';
+export const ERROR_CREATE_USER_MESSAGE = 'Por favor intente nuevamente más tarde.';
