7c48d9898948e63b68430e7c98e171c07e2cec1c
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-07 20:34:09 -0600

    test: add error handling for user registration when email is in use

--- __tests__/register.test.tsx
+++ __tests__/register.test.tsx
@@ -5,7 +5,7 @@ import userEvent from '@testing-library/user-event'
 
 import QueryProviderWrapper from "@/app/QueryProviderWrapper";
 import RegisterPage from "@/app/register/page";
-import { SUCCESS_CREATE_USER_MESSAGE, SUCCESS_CREATE_USER_TITLE } from '@/shared/constants/Global.constants';
+import { ERROR_CREATE_USER_TITLE, SUCCESS_CREATE_USER_MESSAGE, SUCCESS_CREATE_USER_TITLE } from '@/shared/constants/Global.constants';
 
 jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
@@ -56,7 +56,62 @@ describe('Register', () => {
   })
 
   describe('Test creation of user', () => {
-    it.only('should show the result screen after successfully creating a user', async () => {
+    it('Given a user registering his user and the email is in use, show error', async () => {
+      const user = userEvent.setup()
+      mockedAxios.post.mockRejectedValue({
+        code: 'ERR_BAD_REQUEST',
+        config: null,
+        message: 'Request failed with status code 401',
+        name: 'AxiosError',
+        request: null,
+        response: {
+          config: null,
+          data: {
+            data: null,
+            error: {
+              error: 'Unauthorized',
+              message: 'Email or Password incorrect.',
+              statusCode: 401
+            },
+            message: null,
+            success: false,
+            version: '1.2.0'
+          }
+        }
+      })
+
+      render(
+        <QueryProviderWrapper>
+          <RegisterPage />
+        </QueryProviderWrapper>
+      )
+
+      const firstNameInput = screen.getByLabelText(/primer nombre/i)
+      const middleNameInput = screen.getByLabelText(/segundo nombre/i)
+      const lastNameInput = screen.getByLabelText(/apellido/i)
+      const nextButton = screen.getByRole('button', { name: /siguiente/i })
+      await user.type(firstNameInput, 'John')
+      await user.type(middleNameInput, 'Paul')
+      await user.type(lastNameInput, 'Doe')
+      await user.click(nextButton)
+
+      const emailInput = screen.getByLabelText(/correo electrónico/i)
+      const passwordInput = screen.getByTestId('password')
+      const confirmPasswordInput = screen.getByTestId('confirmPassword')
+      const createAccountButton = screen.getByRole('button', { name: /crear cuenta/i })
+      
+      await user.type(emailInput, 'rafa@example.com')
+      await user.type(passwordInput, 'UnaContrase;amuylargaparaminuevacuenta!1')
+      await user.type(confirmPasswordInput, 'UnaContrase;amuylargaparaminuevacuenta!1')
+      await user.click(createAccountButton)
+      expect(await screen.findByText(ERROR_CREATE_USER_TITLE)).toBeInTheDocument()
+      expect(screen.getByText('Intente con otro correo electrónico')).toBeInTheDocument()
+      const goBackLogin = screen.getByRole('link', { name: /regresar al inicio/i })
+      expect(goBackLogin).toBeInTheDocument()
+      expect(screen.getByRole('button', { name: /volver a intentar/i })).toBeInTheDocument()
+    })
+
+    it('should show the result screen after successfully creating a user', async () => {
       const user = userEvent.setup()
       mockedAxios.post.mockResolvedValue({
         error: null,
