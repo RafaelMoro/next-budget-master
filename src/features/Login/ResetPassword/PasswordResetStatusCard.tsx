93bf6a667323a442d2a7eaeb7997def59bb062db
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-09 13:07:46 -0600

    Refactor PasswordResetStatusCard to use ResetPasswordStatus type and enhance layout with gap styling

--- src/features/Login/ResetPassword/PasswordResetStatusCard.tsx
+++ src/features/Login/ResetPassword/PasswordResetStatusCard.tsx
@@ -1,14 +1,15 @@
-import { LOGIN_ROUTE } from "@/shared/constants/Global.constants";
+import { FORGOT_PASSWORD_ROUTE, LOGIN_ROUTE } from "@/shared/constants/Global.constants";
+import { ResetPasswordStatus } from "@/shared/types/Login.types";
 import { LinkButton } from "@/shared/ui/atoms/LinkButton";
 import { Card } from "flowbite-react";
 
 interface PasswordResetStatusCardProps {
-  status: 'success' | 'error';
+  status: ResetPasswordStatus;
 }
 
 export const PasswordResetStatusCard =({ status }: PasswordResetStatusCardProps) => {
   return (
-    <Card className="max-w-[400px]">
+    <Card className="max-w-[400px] gap-8">
       <h5 className="text-2xl font-bold text-center text-gray-900 dark:text-white text-balance">
         { status === "success" ? "🟢 ¡Contraseña cambiada con éxito!" : "🚫 No pudimos restablecer tu contraseña"}
       </h5>
@@ -17,7 +18,8 @@ export const PasswordResetStatusCard =({ status }: PasswordResetStatusCardProps)
           "Tu nueva contraseña ya está activa. Inicia sesión y sigue conquistando tus finanzas."
           : "Parece que hubo un problema. Vuelve a iniciar el proceso desde “Olvidé mi contraseña” y lo resolvemos en segundos." }
       </p>
-      <LinkButton href={LOGIN_ROUTE} text="Regresar al inicio" />
+      { status === "error" && (<LinkButton href={FORGOT_PASSWORD_ROUTE} text="Ir a olvidé mi contraseña" />) }
+      <LinkButton href={LOGIN_ROUTE} isSecondary={status === "error"} text="Regresar al inicio" />
     </Card>
   )
 }
