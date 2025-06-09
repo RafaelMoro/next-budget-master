ff92b928ea8f4f000ad12e6eeb9b17f312f4c400
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-09 12:43:04 -0600

    Add PasswordResetStatusCard component with success and error status handling

--- src/features/Login/ResetPassword/PasswordResetStatusCard.tsx
+++ src/features/Login/ResetPassword/PasswordResetStatusCard.tsx
@@ -1,5 +1,19 @@
-export const PasswordResetStatusCard =() => {
+import { LOGIN_ROUTE } from "@/shared/constants/Global.constants";
+import { LinkButton } from "@/shared/ui/atoms/LinkButton";
+import { Card } from "flowbite-react";
+
+interface PasswordResetStatusCardProps {
+  status: 'success' | 'error';
+}
+
+export const PasswordResetStatusCard =({ status }: PasswordResetStatusCardProps) => {
   return (
-    <div>Message status card</div>
+    <Card className="max-w-[400px]">
+      <h5 className="text-2xl font-bold text-center text-gray-900 dark:text-white text-balance">
+        ¡Contraseña cambiada con éxito!
+      </h5>
+      <p className="text-xl text-black dark:text-white text-pretty">Tu nueva contraseña ya está activa. Inicia sesión y sigue conquistando tus finanzas.</p>
+      <LinkButton href={LOGIN_ROUTE} text="Regresar al inicio" />
+    </Card>
   )
 }
