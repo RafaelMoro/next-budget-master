fd22f12e2ff9339b1e88f8c23ae4dbcf93767f88
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-09 12:46:33 -0600

    Refactor PasswordResetStatusCard to conditionally render success and error messages based on status prop

--- src/features/Login/ResetPassword/PasswordResetStatusCard.tsx
+++ src/features/Login/ResetPassword/PasswordResetStatusCard.tsx
@@ -10,9 +10,13 @@ export const PasswordResetStatusCard =({ status }: PasswordResetStatusCardProps)
   return (
     <Card className="max-w-[400px]">
       <h5 className="text-2xl font-bold text-center text-gray-900 dark:text-white text-balance">
-        ¡Contraseña cambiada con éxito!
+        { status === "success" ? "🟢 ¡Contraseña cambiada con éxito!" : "🚫 No pudimos restablecer tu contraseña"}
       </h5>
-      <p className="text-xl text-black dark:text-white text-pretty">Tu nueva contraseña ya está activa. Inicia sesión y sigue conquistando tus finanzas.</p>
+      <p className="text-xl text-black dark:text-white text-pretty">
+        { status === 'success' ?
+          "Tu nueva contraseña ya está activa. Inicia sesión y sigue conquistando tus finanzas."
+          : "Parece que hubo un problema. Vuelve a iniciar el proceso desde “Olvidé mi contraseña” y lo resolvemos en segundos." }
+      </p>
       <LinkButton href={LOGIN_ROUTE} text="Regresar al inicio" />
     </Card>
   )
