bfed1b5eb421eeb116cfb1489ed1646559408821
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-11 16:19:46 -0600

    Fix onclick issue of dropdown by using the component directly in dropdown select account

--- src/features/Accounts/DropdownSelectAccount.tsx
+++ src/features/Accounts/DropdownSelectAccount.tsx
@@ -1,13 +1,33 @@
-import { ReactElement } from "react"
 import { Dropdown, DropdownItem } from "flowbite-react"
+import { RiExpandUpDownLine } from "@remixicon/react";
 
 interface DropdownSelectAccountProps {
-  customElement: () => ReactElement
+  title: string;
+  amount: string;
+  type: string;
 }
-
-export const DropdownSelectAccount = ({ customElement }: DropdownSelectAccountProps) => {
+/**
+ * This button component is shown in desktop in the aside bar of the dashboard to select other account.
+ * 
+ * Caveats: I cannot have the custom element of the prop renderTrigger in a separate component as the dropdown when clicked,
+ * it does not trigger the onclick event.
+ */
+export const DropdownSelectAccount = ({ title, amount, type }: DropdownSelectAccountProps) => {
   return (
-    <Dropdown label="" renderTrigger={customElement}>
+    <Dropdown aria-label="Select other account" label="" renderTrigger={() => (
+      <article
+        className="flex justify-between w-full items-center gap-x-2.5 rounded-md border border-gray-300 bg-white p-2 text-sm shadow-sm transition-all hover:bg-gray-50 dark:border-gray-800 dark:bg-gray-950 hover:dark:bg-gray-900 focus:ring-2 focus:ring-indigo-200 focus:dark:ring-indigo-700/30 focus:border-indigo-500 focus:dark:border-indigo-700"
+        >
+        <div className="flex flex-col gap-2 items-start">
+          <p className="text-sm">{title}</p>
+          <p className="text-xs text-gray-400">{amount}</p>
+          <span className="bg-blue-100 max-w-min text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-gray-700 dark:text-blue-400 border border-blue-400">
+            {type}
+          </span>
+        </div>
+        <RiExpandUpDownLine className="cursor-pointer" strokeWidth={1} />
+      </article>
+    )}>
       <DropdownItem>HSBC Clasica</DropdownItem>
       <DropdownItem>Santander</DropdownItem>
     </Dropdown>
--- src/shared/ui/organisms/DashboardAside.tsx
+++ src/shared/ui/organisms/DashboardAside.tsx
@@ -5,9 +5,7 @@ import { AccountRecordsIcon } from "../icons/AccountRecordsIcon"
 import { ReactNode } from "react"
 import { DashboardAsideLink } from "../atoms/DashboardAsideLink"
 import { DASHBOARD_ROUTE } from "@/shared/constants/Global.constants"
-import { ChangeAccoubntButton } from "@/features/Accounts/ChangeAccountButton"
 import { DropdownSelectAccount } from "@/features/Accounts/DropdownSelectAccount"
-import { RiExpandUpDownLine } from "@remixicon/react";
 
 interface DashboardAsideProps {
   children: ReactNode;
@@ -18,26 +16,10 @@ export const DashboardAside = ({ children }: DashboardAsideProps) => {
     <aside className="w-72 p-5 flex flex-col gap-4 border-r border-r-gray-600">
       {children}
       <DropdownSelectAccount
-        customElement={() => (
-          <ChangeAccoubntButton
-            title="American Express"
-            amount="$1,000.00"
-            type="Credito"
-          />
-        )}
+        title="American Express"
+        amount="$ 1,000.00"
+        type="Credito"
       />
-      <DropdownSelectAccount customElement={() => (
-        <article className="flex justify-between w-full items-center gap-x-2.5 rounded-md border border-gray-300 bg-white p-2 text-sm shadow-sm transition-all hover:bg-gray-50 dark:border-gray-800 dark:bg-gray-950 hover:dark:bg-gray-900 focus:ring-2 focus:ring-indigo-200 focus:dark:ring-indigo-700/30 focus:border-indigo-500 focus:dark:border-indigo-700">
-          <div className="flex flex-col gap-2 items-start">
-            <p className="text-sm">American express</p>
-            <p className="text-xs text-gray-400">$1,000.00</p>
-            <span className="bg-blue-100 max-w-min text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-gray-700 dark:text-blue-400 border border-blue-400">
-              Credit
-            </span>
-          </div>
-          <RiExpandUpDownLine className="cursor-pointer" strokeWidth={1} />
-        </article>
-        )} />
       <nav className="flex flex-col mt-10">
         <DashboardAsideLink href={DASHBOARD_ROUTE}>
             <HomeIcon />
