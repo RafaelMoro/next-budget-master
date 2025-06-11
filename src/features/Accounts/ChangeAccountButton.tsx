9217c2d1ab7f7a97a416161f1ef01131f96f6b3d
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-11 15:50:51 -0600

    Add ChangeAccountButton component to DashboardAside for improved account display

---
+++ src/features/Accounts/ChangeAccountButton.tsx
@@ -0,0 +1,27 @@
+import { RiExpandUpDownLine } from "@remixicon/react"
+
+interface ChangeAccountButtonProps {
+  title: string;
+  amount: string;
+  type: string;
+}
+
+/**
+ * This button component is shown in desktop in the aside bar of the dashboard
+ */
+export const ChangeAccoubntButton = ({ title, amount, type }: ChangeAccountButtonProps) => {
+  return (
+    <button
+      className="flex justify-between w-full items-center gap-x-2.5 rounded-md border border-gray-300 bg-white p-2 text-sm shadow-sm transition-all hover:bg-gray-50 dark:border-gray-800 dark:bg-gray-950 hover:dark:bg-gray-900 focus:ring-2 focus:ring-indigo-200 focus:dark:ring-indigo-700/30 focus:border-indigo-500 focus:dark:border-indigo-700"
+      >
+      <div className="flex flex-col gap-2 items-start">
+        <p className="text-sm">{title}</p>
+        <p className="text-xs text-gray-400">{amount}</p>
+        <span className="bg-blue-100 max-w-min text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-gray-700 dark:text-blue-400 border border-blue-400">
+          {type}
+        </span>
+      </div>
+      <RiExpandUpDownLine className="cursor-pointer" strokeWidth={1} />
+    </button>
+  )
+}
--- src/shared/ui/organisms/DashboardAside.tsx
+++ src/shared/ui/organisms/DashboardAside.tsx
@@ -5,6 +5,7 @@ import { RiExpandUpDownLine } from "@remixicon/react"
 import { ReactNode } from "react"
 import { DashboardAsideLink } from "../atoms/DashboardAsideLink"
 import { DASHBOARD_ROUTE } from "@/shared/constants/Global.constants"
+import { ChangeAccoubntButton } from "@/features/Accounts/ChangeAccountButton"
 
 interface DashboardAsideProps {
   children: ReactNode;
@@ -14,16 +15,11 @@ export const DashboardAside = ({ children }: DashboardAsideProps) => {
   return (
     <aside className="w-72 p-5 flex flex-col gap-4 border-r border-r-gray-600">
       {children}
-      <button
-        className="flex justify-between w-full items-center gap-x-2.5 rounded-md border border-gray-300 bg-white p-2 text-sm shadow-sm transition-all hover:bg-gray-50 dark:border-gray-800 dark:bg-gray-950 hover:dark:bg-gray-900 focus:ring-2 focus:ring-indigo-200 focus:dark:ring-indigo-700/30 focus:border-indigo-500 focus:dark:border-indigo-700"
-        >
-        <div className="flex flex-col gap-2 items-start">
-          <p className="text-sm">American Express</p>
-          <p className="text-xs text-gray-400">$1,249.00</p>
-          <span className="bg-blue-100 max-w-min text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-gray-700 dark:text-blue-400 border border-blue-400">Credito</span>
-        </div>
-        <RiExpandUpDownLine className="cursor-pointer" strokeWidth={1} />
-      </button>
+      <ChangeAccoubntButton
+        title="American Express"
+        amount="$1,000.00"
+        type="Credito"
+      />
       <nav className="flex flex-col mt-10">
         <DashboardAsideLink href={DASHBOARD_ROUTE}>
             <HomeIcon />
