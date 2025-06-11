9d4e62c64d9aa81cb710d3a4afa16d9edac0fe80
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-11 15:43:04 -0600

    Refactor DashboardAside to include a button for credit card details and improve layout structure

--- src/shared/ui/organisms/DashboardAside.tsx
+++ src/shared/ui/organisms/DashboardAside.tsx
@@ -1,7 +1,7 @@
-import Link from "next/link"
 import { HomeIcon } from "../icons/HomeIcon"
 import { CreditCardIcon } from "../icons/CreditCardIcon"
 import { AccountRecordsIcon } from "../icons/AccountRecordsIcon"
+import { RiExpandUpDownLine } from "@remixicon/react"
 import { ReactNode } from "react"
 import { DashboardAsideLink } from "../atoms/DashboardAsideLink"
 import { DASHBOARD_ROUTE } from "@/shared/constants/Global.constants"
@@ -14,7 +14,17 @@ export const DashboardAside = ({ children }: DashboardAsideProps) => {
   return (
     <aside className="w-72 p-5 flex flex-col gap-4 border-r border-r-gray-600">
       {children}
-      <nav className="flex flex-col">
+      <button
+        className="flex justify-between w-full items-center gap-x-2.5 rounded-md border border-gray-300 bg-white p-2 text-sm shadow-sm transition-all hover:bg-gray-50 dark:border-gray-800 dark:bg-gray-950 hover:dark:bg-gray-900 focus:ring-2 focus:ring-indigo-200 focus:dark:ring-indigo-700/30 focus:border-indigo-500 focus:dark:border-indigo-700"
+        >
+        <div className="flex flex-col gap-2 items-start">
+          <p className="text-sm">American Express</p>
+          <p className="text-xs text-gray-400">$1,249.00</p>
+          <span className="bg-blue-100 max-w-min text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-gray-700 dark:text-blue-400 border border-blue-400">Credito</span>
+        </div>
+        <RiExpandUpDownLine className="cursor-pointer" strokeWidth={1} />
+      </button>
+      <nav className="flex flex-col mt-10">
         <DashboardAsideLink href={DASHBOARD_ROUTE}>
             <HomeIcon />
             Panorama
