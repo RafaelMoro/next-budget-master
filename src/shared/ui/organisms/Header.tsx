6dc982f6bc74400ce1b00ad6b7f0f9df4b49d245
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-11 22:41:35 -0600

    Add mobile header support with HeaderMenuMobile component

--- src/shared/ui/organisms/Header.tsx
+++ src/shared/ui/organisms/Header.tsx
@@ -1,11 +1,24 @@
+"use client"
 import Image from "next/image"
 import { ToggleDarkMode } from "../atoms/ToggleDarkMode"
+import { useMediaQuery } from "@/shared/hooks/useMediaQuery";
+import { HeaderMenuMobile } from "../atoms/HeaderMenuMobile";
 
 interface HeaderProps {
   isHeaderAside?: boolean;
 }
 
 export const Header = ({ isHeaderAside }: HeaderProps) => {
+  const { isMobile } = useMediaQuery()
+
+  if (isMobile && isHeaderAside) {
+    return (
+      <header className="p-4 flex flex-row justify-between items-center">
+        <Image className="rounded-full" src="/img/logo-no-bg.webp" alt="Budget Master Logo" width={70} height={70} />
+        <HeaderMenuMobile />
+      </header>
+    )
+  }
   if (isHeaderAside) {
     return (
       <div className="flex justify-between items-center">
