38b89a3d9d178e799531e8815b057c663edf5e82
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-11 22:46:04 -0600

    add comment

--- src/shared/hooks/useMediaQuery.tsx
+++ src/shared/hooks/useMediaQuery.tsx
@@ -1,5 +1,6 @@
 "use client"
 export const useMediaQuery = () => {
+  // Value obtained based on tailwind where md is equivalent to 768px
   const isMobile = window.matchMedia('(max-width: 768px)').matches;
   return {
     isMobile
