e1026db2b4df2753b6a4684f4a468a90fe9706c1
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-13 02:28:09 -0600

    style: enhance Account component with gradient background and improved text styling

--- src/features/Accounts/Accounts.tsx
+++ src/features/Accounts/Accounts.tsx
@@ -1,5 +1,5 @@
-import { AccountProvider } from "@/shared/types/accounts.types";
 import Image from "next/image"
+import { AccountProvider } from "@/shared/types/accounts.types";
 
 interface AccountProps {
   name: string;
@@ -16,15 +16,15 @@ export const Account = ({ name, balance, accountType, accountProvider = 'masterc
   }
 
   return (
-    <article className="p-2 rounded-3xl card-gradient-bg flex flex-col justify-between max-w-64 min-h-40">
-      <span className="text-lg">{name}</span>
+    <button className="p-2 rounded-3xl card-gradient-bg flex flex-col justify-between max-w-64 min-h-40 text-start cursor-pointer">
+      <span className="text-lg text-gray-200">{name}</span>
       <div>
-        <h5 className="text-2xl font-semibold">{balance}</h5>
+        <h5 className="text-2xl text-gray-200 font-semibold">{balance}</h5>
         <div className="flex justify-between mx-2 mb-1">
-          <p className="text-sm text-gray-400">{accountType} **0762</p>
+          <p className="text-sm text-gray-300 dark:text-gray-400">{accountType} **0762</p>
           <Image src={accProviderImg[accountProvider]} alt="Budget Master Logo" width={40} height={40} />
         </div>
       </div>
-    </article>
+    </button>
   )
 }
