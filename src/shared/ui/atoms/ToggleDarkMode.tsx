36c01ddafc2f3443017916e5ffecce7dacbeac81
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-12 22:47:43 -0600

    feat: enhance ToggleDarkMode component with local storage integration for theme persistence

--- src/shared/ui/atoms/ToggleDarkMode.tsx
+++ src/shared/ui/atoms/ToggleDarkMode.tsx
@@ -1,8 +1,9 @@
 "use client"
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Button } from "flowbite-react";
 import { NightIcon } from "@/shared/ui/icons/Nights";
 import { ThemeMode } from "@/shared/constants/Global.constants";
+import { addToLocalStorage, getLocalStorageInfo } from "@/shared/lib/local-storage.lib";
 
 interface ToggleDarkModeProps {
   cssClass?: string;
@@ -10,17 +11,36 @@ interface ToggleDarkModeProps {
 
 export const ToggleDarkMode = ({ cssClass }: ToggleDarkModeProps) => {
   const [mode, setMode] = useState<ThemeMode>('dark');
+
   const toggleDarkMode = () => {
     const htmlElement = document.documentElement;
 
     if (mode === 'light') {
       setMode('dark')
+      addToLocalStorage({ newInfo: { preferences: { themeMode: 'dark' } } })
       htmlElement.setAttribute("data-theme", "dark");
       return
     }
+
     setMode('light')
+    addToLocalStorage({ newInfo: { preferences: { themeMode: 'light' } } })
     htmlElement.setAttribute("data-theme", "light");
   }
+
+  useEffect(() => {
+    try {
+      const localStorageInfo = getLocalStorageInfo()
+      // Means the local storage is empty
+      if (Object.keys(localStorageInfo).length === 0) {
+        return
+      }
+
+      const { preferences: { themeMode } } = localStorageInfo
+      setMode(themeMode)
+    } catch (error) {
+      console.log('error getting local storage', error)
+    }
+  }, [])
 
   return (
     <Button className={cssClass && cssClass} data-testid="toggle-theme-mode-button" onClick={toggleDarkMode} color="dark" outline>
