666acc038e104d7035ab6beeb853cb6db2d91474
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-18 03:53:10 -0600

    test: update AccountProviderDropdownWrapper to accept provider prop for dynamic rendering

--- __tests__/components/AccountProviderDropdown.test.tsx
+++ __tests__/components/AccountProviderDropdown.test.tsx
@@ -3,8 +3,10 @@ import { useState } from "react"
 import { AccountProviderDropdown } from "@/features/Accounts/AccountProviderDropdown"
 import { AccountProvider } from "@/shared/types/accounts.types"
 
-const AccountProviderDropdownWrapper = () => {
-  const [selectedProvider, setSelectedProvider] = useState<AccountProvider>('mastercard')
+const AccountProviderDropdownWrapper = ({
+  provider = 'mastercard'
+}: { provider?: AccountProvider }) => {
+  const [selectedProvider, setSelectedProvider] = useState<AccountProvider>(provider)
   const changeSelectedProviderType = (newProvider: AccountProvider) => setSelectedProvider(newProvider)
 
   return (
@@ -17,5 +19,17 @@ describe('AccountProviderDropdown', () => {
     render(<AccountProviderDropdownWrapper />)
 
     expect(screen.getByText('Tipo de cuenta: Mastercard')).toBeInTheDocument()
+  })
+
+  it('Given a default provider as visa, Show dropdown with provider visa', () => {
+    render(<AccountProviderDropdownWrapper provider="visa" />)
+
+    expect(screen.getByText('Tipo de cuenta: Visa')).toBeInTheDocument()
+  })
+
+  it('Given a default provider as american-express, Show dropdown with provider american-express', () => {
+    render(<AccountProviderDropdownWrapper provider="american-express" />)
+
+    expect(screen.getByText('Tipo de cuenta: American Express')).toBeInTheDocument()
   })
 })
