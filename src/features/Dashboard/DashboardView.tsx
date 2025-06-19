023298fd5a35adc81e4cfa1419ddb15a1c286945
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-18 21:38:34 -0600

    feat: integrate CreateAccButton into AccountsView and DashboardView for account creation

--- src/features/Dashboard/DashboardView.tsx
+++ src/features/Dashboard/DashboardView.tsx
@@ -51,10 +51,14 @@ export const DashboardView = ({ accounts, detailedError }: DashboardViewProps) =
       </DashboardAside>
       <main className="w-full pl-4 pt-4 min-w-xl mt-3 flex flex-col gap-4">
         <h1 className="text-black dark:text-white text-4xl text-center font-bold col-span-3">Cuentas bancarias</h1>
-        <div className="w-full flex justify-end">
-          <CreateAccButton />
-        </div>
-        { accounts.length > 0  && (<p className="text-center text-xl">Haz click en cualquiera de tus cuentas para ver más en detalle la informacion</p>)}
+        { accounts.length > 0  && (
+          <>
+            <div className="w-full flex justify-end">
+              <CreateAccButton />
+            </div>
+            <p className="text-center text-xl">Haz click en cualquiera de tus cuentas para ver más en detalle la informacion</p>
+          </>
+          )}
         <AccountsView accounts={accounts} />
       </main>
       <Toaster position="top-center" />
