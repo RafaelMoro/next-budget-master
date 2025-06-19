ff0ed3598012906e2d2fa7c9cee0ce2aeba8496a
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-18 20:34:55 -0600

    feat: add tests for modal interactions in DeleteAccount component

--- __tests__/feature/Accounts/DeleteAccount.test.tsx
+++ __tests__/feature/Accounts/DeleteAccount.test.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { render, screen } from "@testing-library/react"
 import { Modal } from "flowbite-react"
+import userEvent from '@testing-library/user-event';
 
 import { AppRouterContextProviderMock } from "@/shared/ui/organisms/AppRouterContextProviderMock"
 import QueryProviderWrapper from "@/app/QueryProviderWrapper"
@@ -61,4 +62,30 @@ describe('DeleteAccount', () => {
     expect(screen.getByText('Estás por eliminar tu cuenta de forma permanente. Si estás completamente seguro, continúa. No podrás recuperar tu información después.')).toBeInTheDocument()
     expect(screen.getByRole('button', { name: /Eliminar/i })).toBeInTheDocument()
   })
+
+  it('Given a user clicking on cancel button, the modal should be closed', async () => {
+      const user = userEvent.setup();
+      const push = jest.fn()
+      const closeModal = jest.fn()
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const updateAccAction = jest.fn((_acc: AccountModalAction) => {})
+      render(<DeleteAccountWrapper closeModal={closeModal} updateAccAction={updateAccAction} push={push} />)
+  
+      const cancelButton = screen.getByRole('button', { name: /Cancelar/i })
+      await user.click(cancelButton)
+      expect(updateAccAction).toHaveBeenCalledWith('view')
+    })
+  
+    it('Given a user clicking on go back button, the modal action should be view', async () => {
+      const user = userEvent.setup();
+      const push = jest.fn()
+      const closeModal = jest.fn()
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const updateAccAction = jest.fn((_acc: AccountModalAction) => {})
+      render(<DeleteAccountWrapper closeModal={closeModal} updateAccAction={updateAccAction} push={push} />)
+  
+      const goBackButton = screen.getByRole('button', { name: /Volver/i })
+      await user.click(goBackButton)
+      expect(updateAccAction).toHaveBeenCalledWith('view')
+    })
 })
