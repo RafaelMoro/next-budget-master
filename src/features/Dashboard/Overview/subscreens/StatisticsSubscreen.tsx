3a51afa7bd105188392772f69c6b158cc939bbba
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-20 17:39:08 -0600

    refactor: enhance no records message with improved UI and button to register movement

--- src/features/Dashboard/Overview/subscreens/StatisticsSubscreen.tsx
+++ src/features/Dashboard/Overview/subscreens/StatisticsSubscreen.tsx
@@ -1,6 +1,7 @@
 import { IncomeExpensesChart } from "../../../Charts/IncomeExpensesChart"
 import { NO_RECORDS_FOUND } from "@/shared/constants/records.constants";
 import { useDashboardStore } from "@/zustand/provider/dashboard-store-provider";
+import { Button } from "flowbite-react";
 
 interface StatisticsViewProps {
   message: string | null;
@@ -12,9 +13,12 @@ export const StatisticsSubscreen = ({ message }: StatisticsViewProps) => {
   )
   if (message === NO_RECORDS_FOUND) {
     return (
-      <div>
-        <h2>No has creado transacciones en esta cuenta para este mes.</h2>
-        <p>Empieza a crear una ahora</p>
+      <div className="flex flex-col justify-center gap-5 mt-20">
+        <h2 className="text-2xl font-semibold text-center">Sin transacciones, sin gráficas... ¡por el momento!</h2>
+        <p className="text-gray-400 text-center mb-5">Aún no podemos generar gráficas sin movimientos... ¡empieza a registrar y verás el cambio!</p>
+        <div className="mx-auto my-0">
+          <Button>Registrar movimiento</Button>
+        </div>
       </div>
     )
   }
