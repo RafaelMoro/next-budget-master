ee4bf1723269b11d9d48932cb0b888d1738ee3d3
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-20 14:04:52 -0600

    feat: add IncomeExpensesChart component and utility function for calculating income and expenses totals

---
+++ src/features/Charts/IncomeExpensesChart.tsx
@@ -0,0 +1,26 @@
+import { BankMovement } from "@/shared/types/records.types";
+import { AreaChart } from "@/shared/ui/tremor/AreaChart"
+import { getExpensesIncomesTotal } from "@/shared/utils/statistics.utils";
+import { Card } from "flowbite-react";
+
+interface IncomeExpensesChartProps {
+  records: BankMovement[]
+}
+
+export const IncomeExpensesChart = ({ records }: IncomeExpensesChartProps) => {
+  const currentData = getExpensesIncomesTotal(records);
+  console.log('currentData', currentData)
+
+  return (
+    <Card className="flex flex-col gap-2">
+      <h4 className="text-2xl font-bold">Income vs Expenses</h4>
+      <AreaChart
+        className="h-52"
+        data={currentData}
+        index="fullDate"
+        categories={["expenseTotal", "incomeTotal"]}
+        showLegend={false}
+      />
+    </Card>
+  )
+}
