3cce6219c5b380151dcde364eb1918040f3fd495
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-20 12:10:42 -0600

    feat: update AccountScreen to display NoAccountsFoundView when no accounts are present

--- src/features/Accounts/AccountScreen.tsx
+++ src/features/Accounts/AccountScreen.tsx
@@ -1,6 +1,7 @@
 import { AccountBank } from "@/shared/types/accounts.types";
 import { CreateAccButton } from "./CreateAccButton";
 import { AccountsView } from "./AccountsView";
+import { NoAccountsFoundView } from "./NoAccountsFoundView";
 
 interface AccountScreenProps {
   accounts: AccountBank[];
@@ -15,15 +16,18 @@ export const AccountScreen = ({ accounts }: AccountScreenProps) => {
   return (
     <main className="w-full pl-4 pt-4 min-w-xl mt-3 flex flex-col gap-4">
       <h1 className="text-black dark:text-white text-4xl text-center font-bold col-span-3">Cuentas bancarias</h1>
+      { accounts.length === 0 && (
+        <NoAccountsFoundView />
+      )}
       { accounts.length > 0  && (
         <>
           <div className="w-full flex justify-end">
             <CreateAccButton />
           </div>
           <p className="text-center text-xl mb-5">Haz click en cualquiera de tus cuentas para ver más en detalle la informacion</p>
+          <AccountsView accounts={accounts} />
         </>
-        )}
-      <AccountsView accounts={accounts} />
+      )}
     </main>
   )
 }
--- src/features/Accounts/AccountsView.tsx
+++ src/features/Accounts/AccountsView.tsx
@@ -8,7 +8,6 @@ import { Modal } from "flowbite-react";
 import { AccountDetails } from "./AccountDetails";
 import { EditAccount } from "./EditAccount";
 import { DeleteAccount } from "./DeleteAccount";
-import { NoAccountsFoundView } from "./NoAccountsFoundView";
 
 interface AccountsViewProps {
   accounts: AccountBank[];
@@ -57,46 +56,45 @@ export const AccountsView = ({ accounts }: AccountsViewProps) => {
     }
   }, [accounts])
 
-  if (accountsDisplay.length > 0) {
-    return (
-      <section className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        { accountsDisplay.map((acc) => (
-          <Account
-            key={acc.accountId}
-            account={acc}
-            openModal={openModal}
-          />
-        )) }
-        { accDetails && (
-          <Modal show={openAccModal} onClose={closeModal}>
-            { accAction === 'view' && (
-              <AccountDetails
-                account={accDetails}
-                updateAccAction={updateAccAction}
-              />
-            )}
-            { accAction === 'edit' && (
-              <EditAccount
-                account={accDetails}
-                closeModal={closeModal}
-                updateAccAction={updateAccAction}
-              />
-            )}
-
-            { accAction === 'delete' && (
-              <DeleteAccount
-                account={accDetails}
-                closeModal={closeModal}
-                updateAccAction={updateAccAction}
-              />
-            )}
-          </Modal>
-        ) }
-      </section>
-    )
+  if (accountsDisplay.length === 0) {
+    return null
   }
 
   return (
-    <NoAccountsFoundView />
+    <section className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      { accountsDisplay.map((acc) => (
+        <Account
+          key={acc.accountId}
+          account={acc}
+          openModal={openModal}
+        />
+      )) }
+      { accDetails && (
+        <Modal show={openAccModal} onClose={closeModal}>
+          { accAction === 'view' && (
+            <AccountDetails
+              account={accDetails}
+              updateAccAction={updateAccAction}
+            />
+          )}
+          { accAction === 'edit' && (
+            <EditAccount
+              account={accDetails}
+              closeModal={closeModal}
+              updateAccAction={updateAccAction}
+            />
+          )}
+
+          { accAction === 'delete' && (
+            <DeleteAccount
+              account={accDetails}
+              closeModal={closeModal}
+              updateAccAction={updateAccAction}
+            />
+          )}
+        </Modal>
+      ) }
+    </section>
   )
+  
 }
