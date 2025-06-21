d64410f1a42dadac09ecd3d97432f43c25a036a2
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-21 13:09:17 -0600

    add ignore of mocks

--- jest.config.ts
+++ jest.config.ts
@@ -168,9 +168,9 @@ const config: Config = {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  // testPathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  testPathIgnorePatterns: [
+    "<rootDir>/__tests__/mocks/"
+  ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
