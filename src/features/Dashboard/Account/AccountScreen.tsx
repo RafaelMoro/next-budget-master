599d63f780f6e088c638a0112362a51ef2b2a2fc
Rafael Moro G <rafael.moro.galindo@gmail.com>
2025-06-22 21:04:02 -0600

    Add account screen to mobile

--- src/features/Dashboard/Account/AccountScreen.tsx
+++ src/features/Dashboard/Account/AccountScreen.tsx
@@ -8,9 +8,9 @@ import { AccountsView } from "../../Accounts/AccountsView";
  */
 export const AccountScreen = () => {
   return (
-    <main className="w-full pl-4 pt-4 min-w-xl mt-3 flex flex-col gap-4">
+    <main className="w-full pl-4 pt-4 md:min-w-xl mt-3 flex flex-col gap-4">
       <h1 className="text-black dark:text-white text-4xl text-center font-bold col-span-3">Cuentas bancarias</h1>
-      <div className="w-full flex justify-end">
+      <div className="w-full flex justify-end mt-10 mb-5 lg:my-0">
         <CreateAccButton />
       </div>
       <p className="text-center text-xl mb-5">Haz click en cualquiera de tus cuentas para ver más en detalle la informacion</p>
