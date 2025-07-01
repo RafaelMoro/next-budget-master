99c21c90ab22a1d75c63d4ef13b59ea3e4488112
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-30 21:55:55 -0600

    feat: implement useSelectAccount hook for account selection logic

--- src/features/Accounts/SelectAccountDialog.tsx
+++ src/features/Accounts/SelectAccountDialog.tsx
@@ -1,10 +1,6 @@
 "use client"
-import { fetchRecordsCurrentMonth } from "@/shared/lib/dashboard.lib";
-import { AccountsDisplay } from "@/shared/types/accounts.types";
-import { saveAccountApi } from "@/shared/utils/user-info.utils";
-import { useDashboardStore } from "@/zustand/provider/dashboard-store-provider";
+import { useSelectAccount } from "@/shared/hooks/useSelectAccount";
 import { Modal, ModalBody, ModalHeader } from "flowbite-react"
-import { useEffect, useState } from "react";
 
 interface SelectAccountProps {
   openModal: boolean;
@@ -12,36 +8,8 @@ interface SelectAccountProps {
 }
 
 export const SelectAccountDialog = ({ openModal, closeModal }: SelectAccountProps) => {
-  const { accountsDisplay, selectedAccountDisplay, updateSelectedAccountDisplay, updateRecords } = useDashboardStore(
-      (state) => state
-    )
-    const [accountsOptions, setAccountsOptions] = useState<AccountsDisplay[]>([])
-  
-    useEffect(() => {
-      if (selectedAccountDisplay) {
-        const options = accountsDisplay
-          .filter(acc => acc.accountId !== selectedAccountDisplay.accountId)
-        setAccountsOptions(options);
-      }
-    }, [accountsDisplay, selectedAccountDisplay])
-  
-    const handleSelectAccount = async (accountId: string) => {
-      const selected = accountsOptions.find(acc => acc.accountId === accountId)
-      if (!selected) {
-        console.warn('Account not found in options:', accountId);
-        return;
-      }
-      const newOptions = accountsDisplay.filter(acc => acc.accountId !== accountId)
-      // Save the account selected into the cookie
-      await saveAccountApi(selected.accountId)
-      setAccountsOptions(newOptions)
-      updateSelectedAccountDisplay(selected)
-  
-      // Fetch new records of the selected account
-      const { records } = await fetchRecordsCurrentMonth({ accountId: selected.accountId });
-      updateRecords(records);
-      closeModal()
-    }
+  const { accountsOptions, handleSelectAccount } = useSelectAccount({ closeModal })
+
   return (
     <Modal show={openModal} onClose={closeModal}>
       <ModalHeader>Seleccione su cuenta:</ModalHeader>
