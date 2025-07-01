b7161634e4fc33317fc4a410b6cd8c9a8c88d7e9
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-30 21:48:38 -0600

    fix: limit account options to a maximum of 10 in SelectAccountDialog

--- src/features/Accounts/SelectAccountDialog.tsx
+++ src/features/Accounts/SelectAccountDialog.tsx
@@ -21,6 +21,7 @@ export const SelectAccountDialog = ({ openModal, closeModal }: SelectAccountProp
       if (selectedAccountDisplay) {
         const options = accountsDisplay
           .filter(acc => acc.accountId !== selectedAccountDisplay.accountId)
+          .slice(0, 10)
         setAccountsOptions(options);
       }
     }, [accountsDisplay, selectedAccountDisplay])
