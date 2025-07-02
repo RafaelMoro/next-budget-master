043eba01ae8a2fa45823cd4cafc86bb9e65a5482
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-07-02 15:14:42 -0600

    feat: integrate CurrencyField and enhance form layout in TransactionManager

--- src/features/Records/TransactionManager.tsx
+++ src/features/Records/TransactionManager.tsx
@@ -1,15 +1,23 @@
 "use client"
 import { useState } from "react"
+
 import { TransactionManagerGroupButton } from "./TransactionManagerGroupButton"
 import { TransactionScreens } from "@/shared/types/dashboard.types"
 import { DateTimePicker } from "@/shared/ui/atoms/DatetimePicker"
 import { Header } from "@/shared/ui/organisms/Header"
+import { useCurrencyField } from "@/shared/hooks/useCurrencyField"
+import { CurrencyField } from "@/shared/ui/atoms/CurrencyField"
+import { Label, Textarea, TextInput } from "flowbite-react"
 
 export const TransactionManager = () => {
   const [subscreen, setSubscreen] = useState<TransactionScreens>('expense')
   const updateExpenseScreen = () => setSubscreen('expense')
   const updateIncomeScreen = () => setSubscreen('income')
   const updateTransferScreen = () => setSubscreen('transfer')
+
+  const { handleChange, currencyState } = useCurrencyField({
+    amount: null
+  })
 
   const titleDictionary: Record<TransactionScreens, string> = {
     expense: 'Gasto',
@@ -20,7 +28,7 @@ export const TransactionManager = () => {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <main className="flex-1 flex flex-col items-center gap-5 min-h-full">
+      <main className="flex-1 flex flex-col items-center gap-8 min-h-full">
         <h1 className="text-black dark:text-white text-4xl text-center font-bold">Crear {titleDictionary[subscreen]}</h1>
         <TransactionManagerGroupButton
           screen={subscreen}
@@ -28,8 +36,35 @@ export const TransactionManager = () => {
           updateIncomeScreen={updateIncomeScreen}
           updateTransferScreen={updateTransferScreen}
         />
-        <form className="max-w-xs mx-auto">
+        <form className="max-w-xs mx-auto flex flex-col gap-4">
           <DateTimePicker />
+          <CurrencyField
+            labelName="Cantidad"
+            dataTestId="amount"
+            fieldId="amount"
+            value={currencyState}
+            handleChange={handleChange}
+          />
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="shortDescription">Pequeña descripción</Label>
+            </div>
+            <TextInput
+              data-testid="shortDescription"
+              id="shortDescription"
+              type="text"
+              // {...register("title")}
+              />
+            {/* { errors?.title?.message && (
+              <ErrorMessage isAnimated>{errors.title?.message}</ErrorMessage>
+            )} */}
+          </div>
+          <div className="max-w-md">
+            <div className="mb-2 block">
+              <Label htmlFor="description">Descripción (opcional)</Label>
+            </div>
+            <Textarea id="description" required rows={4} />
+          </div>
         </form>
       </main>
     </div>
