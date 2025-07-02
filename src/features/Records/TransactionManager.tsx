ccceb88c63c1c3da49f2563fc9f49f2f3620e83c
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-07-02 15:00:41 -0600

    feat: restructure TransactionManager layout and add Header component

--- src/features/Records/TransactionManager.tsx
+++ src/features/Records/TransactionManager.tsx
@@ -3,6 +3,7 @@ import { useState } from "react"
 import { TransactionManagerGroupButton } from "./TransactionManagerGroupButton"
 import { TransactionScreens } from "@/shared/types/dashboard.types"
 import { DateTimePicker } from "@/shared/ui/atoms/DatetimePicker"
+import { Header } from "@/shared/ui/organisms/Header"
 
 export const TransactionManager = () => {
   const [subscreen, setSubscreen] = useState<TransactionScreens>('expense')
@@ -10,18 +11,28 @@ export const TransactionManager = () => {
   const updateIncomeScreen = () => setSubscreen('income')
   const updateTransferScreen = () => setSubscreen('transfer')
 
+  const titleDictionary: Record<TransactionScreens, string> = {
+    expense: 'Gasto',
+    income: 'Ingreso',
+    transfer: 'Transferencia',
+  }
+
   return (
-    <>
-      <TransactionManagerGroupButton
-        screen={subscreen}
-        updateExpenseScreen={updateExpenseScreen}
-        updateIncomeScreen={updateIncomeScreen}
-        updateTransferScreen={updateTransferScreen}
-      />
-      <form className="max-w-xs mx-auto">
-        <DateTimePicker />
-      </form>
-    </>
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-1 flex flex-col items-center gap-5 min-h-full">
+        <h1 className="text-black dark:text-white text-4xl text-center font-bold">Crear {titleDictionary[subscreen]}</h1>
+        <TransactionManagerGroupButton
+          screen={subscreen}
+          updateExpenseScreen={updateExpenseScreen}
+          updateIncomeScreen={updateIncomeScreen}
+          updateTransferScreen={updateTransferScreen}
+        />
+        <form className="max-w-xs mx-auto">
+          <DateTimePicker />
+        </form>
+      </main>
+    </div>
 
   )
 }
