2e3d31060a4f2e41582592cd016d6ff1fdf75889
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-07-04 10:19:21 -0600

    feat: add mock categories data for testing purposes

---
+++ __tests__/mocks/categories.mock.ts
@@ -0,0 +1,19 @@
+import { Category } from "@/shared/types/categories.types";
+
+export const mockCategories: Category[] = [
+  {
+    _id: "65f3be727c46232bf19e7d00",
+    categoryName: "Comida y Bebida",
+    subCategories: [
+        "Bar",
+        "Alcohol & Cigarros",
+        "Comida para llevar",
+        "Comida rápida",
+        "Cafetería",
+        "Restaurantes",
+        "Despensa"
+    ],
+    icon: "foodAndDrink",
+    __v: 0
+  }
+]
