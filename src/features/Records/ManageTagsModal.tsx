14a010cc614cd1fa8f5cc7d58b6a4e5396380d82
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-07-04 21:53:13 -0600

    refactor: enhance ManageTagsModal layout and styling for better user experience

--- src/features/Records/ManageTagsModal.tsx
+++ src/features/Records/ManageTagsModal.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Badge, Button, Modal, ModalBody, ModalFooter, ModalHeader, TextInput } from "flowbite-react"
+import { Badge, Button, Label, Modal, ModalBody, ModalFooter, ModalHeader, TextInput } from "flowbite-react"
 import { useState } from "react"
 
 interface ManageTagsModalProps {
@@ -17,18 +17,26 @@ export const ManageTagsModal = ({ tags, updateTags }: ManageTagsModalProps) => {
       <Modal show={openModal} onClose={() => setOpenModal(false)}>
         <ModalHeader>Agregar etiqueta</ModalHeader>
         <ModalBody>
-          <h3>Etiquetas:</h3>
+          <h3 className="text-2xl text-center font-semibold">Etiquetas:</h3>
           { tags.length > 0 && tags.map((tag) => (
             <Badge key={tag} color="purple">{tag}</Badge>
           )) }
-          <form>
-            <TextInput
-              data-testid="tag"
-              id="tag"
-              type="text"
-              // {...register("shortDescription")}
-            />
-            <Button>Agregar etiqueta</Button>
+          <form className="flex flex-col gap-4 items-center mt-8">
+             <div>
+              <div className="mb-2 block">
+                <Label htmlFor="tag">Etiqueta</Label>
+              </div>
+              <TextInput
+                data-testid="tag"
+                id="tag"
+                type="text"
+                // {...register("shortDescription")}
+              />
+              {/* { errors?.shortDescription?.message && (
+                <ErrorMessage isAnimated>{errors.shortDescription?.message}</ErrorMessage>
+              )} */}
+            </div>
+            <Button className="max-w-max">Agregar etiqueta</Button>
           </form>
         </ModalBody>
         <ModalFooter>
