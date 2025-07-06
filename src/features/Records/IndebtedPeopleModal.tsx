96ddb0a796448734904080b5fd1df0d388d5b754
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-07-06 11:02:10 -0600

    feat: enhance IndebtedPeopleModal with currency fields and toggle switch for debt status

--- src/features/Records/IndebtedPeopleModal.tsx
+++ src/features/Records/IndebtedPeopleModal.tsx
@@ -1,7 +1,10 @@
 "use client"
-
-import { Button, Label, Modal, ModalBody, ModalFooter, ModalHeader, TextInput } from "flowbite-react"
+import { Button, Label, Modal, ModalBody, ModalHeader, TextInput, ToggleSwitch  } from "flowbite-react"
 import { AnimatePresence } from "motion/react"
+
+import { useCurrencyField } from "@/shared/hooks/useCurrencyField"
+import { CurrencyField } from "@/shared/ui/atoms/CurrencyField"
+import { useState } from "react"
 
 interface IdebtedPeopleModalProps {
   openModal: boolean
@@ -9,6 +12,26 @@ interface IdebtedPeopleModalProps {
 }
 
 export const IndebtedPeopleModal = ({ openModal, toggleModal }: IdebtedPeopleModalProps) => {
+  const {
+    handleChange: handleChangeAmountOwed,
+    currencyState: amountOwed,
+    errorAmount: errorAmountOwed,
+    updateErrorAmount: updateErrorAmountOwed,
+  } = useCurrencyField({
+    amount: null,
+  })
+  const {
+    handleChange: handleChangeAmountPaid,
+    currencyState: amountPaid,
+    errorAmount: errorAmountPaid,
+    updateErrorAmount: updateErrorAmountPaid,
+  } = useCurrencyField({
+    amount: null,
+  })
+
+  const [debtPaid, setDebtPaid] = useState<boolean>(false)
+  const toggleDebtPaid = () => setDebtPaid((prev) => !prev)
+
   return (
     <section className="flex flex-col gap-4">
       <p className="text-sm text-gray-600 dark:text-gray-400">
@@ -19,10 +42,10 @@ export const IndebtedPeopleModal = ({ openModal, toggleModal }: IdebtedPeopleMod
         <Modal key="add-indebted-people-modal" show={openModal} onClose={toggleModal}>
           <ModalHeader>Agregar persona que te debe</ModalHeader>
           <ModalBody>
-            <form className="flex flex-col gap-4 items-center mt-8">
+            <form className="flex flex-col gap-4 items-center">
               <div>
                 <div className="mb-2 block">
-                  <Label htmlFor="tag">Etiqueta</Label>
+                  <Label htmlFor="tag">Nombre completo</Label>
                 </div>
                 <TextInput
                   data-testid="name"
@@ -32,16 +55,25 @@ export const IndebtedPeopleModal = ({ openModal, toggleModal }: IdebtedPeopleMod
                 {/* { error && (
                   <ErrorMessage isAnimated={false}>{error}</ErrorMessage>
                 )} */}
-                <Button type="submit" className="max-w-max">Agregar persona</Button>
               </div>
+              <CurrencyField
+                labelName="Cantidad a deber"
+                dataTestId="amountOwed"
+                fieldId="amountOwed"
+                value={amountOwed}
+                handleChange={handleChangeAmountOwed}
+              />
+              <CurrencyField
+                labelName="Cantidad pagada"
+                dataTestId="amountPaid"
+                fieldId="amountPaid"
+                value={amountPaid}
+                handleChange={handleChangeAmountPaid}
+              />
+              <ToggleSwitch checked={debtPaid} label="Deuda pagada" onChange={toggleDebtPaid} />
+              <Button type="submit" className="max-w-max">Agregar persona</Button>
             </form>
           </ModalBody>
-          <ModalFooter>
-            <div className="w-full flex justify-between">
-              <Button color="red">Cancelar</Button>
-              <Button color="green">Finalizar</Button>
-            </div>
-          </ModalFooter>
         </Modal>
       </AnimatePresence>
     </section>
