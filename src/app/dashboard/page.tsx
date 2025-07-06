31f547a83ee1683d125993dcd4ae8ebf05ebff54
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-07-05 18:45:06 -0600

    feat: add metadata constants for login and dashboard pages

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,17 @@
+import type { Metadata } from "next";
+
 import { getAccessToken } from '@/shared/lib/auth.lib';
 import { fetchAccounts, fetchRecordsCurrentMonth } from '@/shared/lib/dashboard.lib';
 import { LoginRequiredModal } from '@/shared/ui/organisms/LoginRequiredModal';
 import { Dashboard } from '@/features/Dashboard/Dashboard';
 import { getAccountCookie } from '@/shared/lib/preferences.lib';
 import { DashboardStoreProvider } from '@/zustand/provider/dashboard-store-provider';
+import { DASHBOARD_META_DESCRIPTION, DASHBOARD_META_TITLE } from "@/shared/constants/metadata.constants";
+
+export const metadata: Metadata = {
+  title: DASHBOARD_META_TITLE,
+  description: DASHBOARD_META_DESCRIPTION,
+};
 
 export default async function DashboardPage () {
   const accessToken = await getAccessToken()
---
+++ src/shared/constants/metadata.constants.ts
@@ -0,0 +1,11 @@
+export const LOGIN_META_TITLE = 'Budget Master | Login - La mejor app de presupuesto familiar';
+export const LOGIN_META_DESCRIPTION = 'Organiza tus finanzas con la mejor app de presupuesto familiar. Controla gastos, ahorra más y mejora tu economía. ¡Inicia sesión ahora!';
+
+export const REGISTER_META_TITLE = 'Budget Master | Registrate: App finanzas personales para iOS';
+export const REGISTER_META_DESCRIPTION = 'Gestiona tu dinero con la mejor app de finanzas personales para iPhone listo para la web. Registra gastos, y crea presupuestos en la palma de tu mano.';
+
+export const FORGOT_PASSWORD_META_TITLE = 'Budget Master | ¿Olvide mi contraseña?';
+export const FORGOT_PASSWORD_META_DESCRIPTION = ' Recupera el acceso a tu herramienta de control de gastos para PC gratis y sigue gestionando tu dinero de manera sencilla y segura. ¡Hazlo en segundos!';
+
+export const DASHBOARD_META_TITLE = ' Budget Master | Panel de administración';
+export const DASHBOARD_META_DESCRIPTION = 'Bienvenido al best personal expense tracker app. Desde este panel de administración organiza tus finanzas y analiza gastos con facilidad. ¡Accede ahora!';
