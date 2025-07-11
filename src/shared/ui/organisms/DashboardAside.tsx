7e784d2a27fa8b01677cb5e2d4707cc1c9a17ced
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-07-11 00:04:36 -0600

    feat: implement record creation functionality in DashboardAside component

--- src/shared/ui/organisms/DashboardAside.tsx
+++ src/shared/ui/organisms/DashboardAside.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react"
+import { useRouter } from 'next/navigation'
 
 import { HomeIcon } from "../icons/HomeIcon"
 import { CreditCardArrowIcon } from "../icons/CreditCardArrowIcon"
@@ -9,6 +10,10 @@ import { AccountBank } from "@/shared/types/accounts.types"
 import { LinkButton } from "../atoms/LinkButton"
 import { DashboardScreens } from "@/shared/types/dashboard.types"
 import { CREATE_RECORD_ROUTE } from "@/shared/constants/Global.constants"
+import { getAccountCookie } from "@/shared/lib/preferences.lib"
+import { useDashboardStore } from "@/zustand/provider/dashboard-store-provider"
+import { saveAccountApi } from "@/shared/utils/user-info.utils"
+import { Button } from "flowbite-react"
 
 interface DashboardAsideProps {
   children: ReactNode;
@@ -19,15 +24,30 @@ interface DashboardAsideProps {
 }
 
 export const DashboardAside = ({ children, accounts, updateScreen, toggleSelectAccountModal, screen }: DashboardAsideProps) => {
+  const router = useRouter()
+  const selectedAccountId = useDashboardStore(
+      (state) => state.selectedAccount?._id
+    )
+  const handleCreateRecord = async () => {
+    try {
+      const selectedAccountCookie = await getAccountCookie()
+      if (!selectedAccountCookie && selectedAccountId) {
+        await saveAccountApi(selectedAccountId)
+      }
+      router.push(CREATE_RECORD_ROUTE)
+    } catch (error) {
+      console.error('Error creating record:', error)
+    }
+  }
   return (
     <aside className="w-72 p-5 flex flex-col gap-4 border-r border-r-gray-600">
       {children}
       { accounts.length > 0 && (
         <DropdownSelectAccount goAccounts={toggleSelectAccountModal} />
       )}
-      <LinkButton
-        href={CREATE_RECORD_ROUTE}
-      >Registrar movimiento</LinkButton>
+      <Button
+        onClick={handleCreateRecord}
+      >Registrar movimiento</Button>
       <nav className="mt-10 flex flex-col">
         <DashboardAsideLink isSelected={screen === 'overview'} onClickCb={() => updateScreen('overview')}>
             <HomeIcon />
