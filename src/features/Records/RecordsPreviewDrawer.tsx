36fa3504b28d3bb7430926d801e34656b71f97cd
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-07-12 11:08:45 -0600

    Refactor RecordsPreviewDrawer for improved structure and mobile responsiveness

--- src/features/Records/RecordsPreviewDrawer.tsx
+++ src/features/Records/RecordsPreviewDrawer.tsx
@@ -1,9 +1,12 @@
 "use client"
 
-import { BankMovement } from "@/shared/types/records.types";
 import { RiDeleteBinFill, RiPencilLine } from "@remixicon/react";
-import { Drawer, DrawerHeader, DrawerItems } from "flowbite-react";
+import { Badge, Drawer, DrawerHeader, DrawerItems } from "flowbite-react";
 import clsx from "clsx"
+
+import { BankMovement } from "@/shared/types/records.types";
+import { categoryIcons } from "@/shared/constants/categories.constants";
+import { useMediaQuery } from "@/shared/hooks/useMediaQuery";
 
 interface RecordsPreviewDrawerProps {
   record: BankMovement | null;
@@ -12,34 +15,53 @@ interface RecordsPreviewDrawerProps {
 }
 
 export const RecordsPreviewDrawer = ({ open, handleClose, record }: RecordsPreviewDrawerProps) => {
+  const { isMobile } = useMediaQuery()
+
+  const Icon = categoryIcons[record?.category?.icon ?? 'newCategory']
+  const priceStyles = clsx(
+    "text-xl font-semibold text-center",
+    { "text-red-600": record?.typeOfRecord === 'expense'},
+    { "text-green-400": record?.typeOfRecord === 'income' },
+    { "text-blue-400": record?.typeOfRecord === 'transfer' }
+  )
+  const drawerDirection = isMobile ? 'bottom' : 'right'
+  
   if (!record) {
     return null;
   }
-  const priceStyles = clsx(
-    "text-xl font-semibold text-center",
-    { "text-red-600": record.typeOfRecord === 'expense'},
-    { "text-green-400": record.typeOfRecord === 'income' },
-    { "text-blue-400": record.typeOfRecord === 'transfer' }
-  )
 
   return (
-    <Drawer open={open} onClose={handleClose} position="right">
+    <Drawer open={open} onClose={handleClose} position={drawerDirection}>
       <DrawerHeader title={`${record.fullDate} ${record.formattedTime}`} />
       <DrawerItems>
-        <div className="flex flex-col gap-4">
-          <h4 className="text-2xl font-bold text-center">{record.shortName}</h4>
-          <div className="flex justify-center gap-3">
-            <button aria-label={`Edit record ${record.shortName}`}className="cursor-pointer mr-3">
-              <RiPencilLine size={18}  />
-            </button>
-            <button aria-label={`Remove record ${record.shortName}`}className="cursor-pointer mr-3">
-              <RiDeleteBinFill size={18} />
-            </button>
-          </div>
+        <div className="flex justify-center gap-3 mb-8">
+          <button aria-label={`Edit record ${record.shortName}`}className="cursor-pointer mr-3">
+            <RiPencilLine size={18}  />
+          </button>
+          <button aria-label={`Remove record ${record.shortName}`}className="cursor-pointer mr-3">
+            <RiDeleteBinFill size={18} />
+          </button>
         </div>
 
-        <div className="mt-10">
+        <div className="flex flex-col gap-4">
+          <h4 className="text-2xl font-bold text-center">{record.shortName}</h4>
+          <div className="flex justify-center gap-2 text-sm text-gray-400">
+            <Icon />
+            <p>{record.category?.categoryName} / {record.subCategory}</p>
+          </div>
           <p className={priceStyles}>{record.amountFormatted}</p>
+          <p className="text-sm text-gray-400">{record.description}</p>
+
+          { record.tag.length > 0 && (
+            <div>
+              <h5>Etiquetas:</h5>
+              { record.tag.map((t) => (
+                <Badge key={t} className="max-w-max" color="purple">
+                  {t}
+                </Badge>
+              )) }
+            </div>
+          ) }
         </div>
       </DrawerItems>
     </Drawer>
