e269a0780424271496366a895752226eec42575a
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-07-12 09:41:10 -0600

    Add form validation tests to IncomeTemplate for description length requirements

--- __tests__/feature/Records/IncomeTemplate.test.tsx
+++ __tests__/feature/Records/IncomeTemplate.test.tsx
@@ -1,4 +1,6 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import { QueryProviderWrapper } from '@/app/QueryProviderWrapper';
 import { AppRouterContextProviderMock } from '@/shared/ui/organisms/AppRouterContextProviderMock';
 import { IncomeTemplate } from '@/features/Records/IncomeTemplate';
@@ -64,4 +66,45 @@ describe('IncomeTemplate', () => {
     expect(screen.getByRole('link', { name: /Cancelar/i })).toBeInTheDocument();
     expect(screen.getByRole('button', { name: /Crear ingreso/i })).toBeInTheDocument();
   });
+
+  describe('Form validations' , () => {
+    it('Given a user clicking on create expense, show validation error for short description to be required', async () => {
+      const user = userEvent.setup();
+      const push = jest.fn();
+      render(<IncomeTemplateWrapper push={push} />);
+
+      const createExpenseButton = screen.getByRole('button', { name: /Crear ingreso/i });
+      await user.click(createExpenseButton);
+
+      expect(screen.getByText(/Por favor, ingrese una pequeña descripción/i)).toBeInTheDocument()
+    })
+
+    it('Given a user typing one letter and then clicking create expense, show validation error for description to be more than 3 characters', async () => {
+      const user = userEvent.setup();
+      const push = jest.fn();
+      render(<IncomeTemplateWrapper push={push} />);
+
+      const description = screen.getByLabelText(/Descripción \(opcional\)/i);
+      await user.type(description, 'a');
+
+      const createExpenseButton = screen.getByRole('button', { name: /Crear ingreso/i });
+      await user.click(createExpenseButton);
+
+      expect(screen.getByText(/Por favor, ingrese una descripción de más de 3 caracteres/i)).toBeInTheDocument();
+    })
+
+    it('Given a user typing more than 300 characters and then clicking create expense, show validation error for description to be less than 300 characters', async () => {
+      const user = userEvent.setup();
+      const push = jest.fn();
+      render(<IncomeTemplateWrapper push={push} />);
+
+      const description = screen.getByLabelText(/Descripción \(opcional\)/i);
+      await user.type(description, 'a'.repeat(301));
+
+      const createExpenseButton = screen.getByRole('button', { name: /Crear ingreso/i });
+      await user.click(createExpenseButton);
+
+      expect(screen.getByText(/Por favor, ingrese una descripción con menos de 300 caracteres/i)).toBeInTheDocument();
+    })
+  })
 });
