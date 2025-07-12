15cfd2541490aa0ccebe41dc765e4058851a15a9
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-07-12 10:40:47 -0600

    Refactor RecordsPreviewDrawer layout for improved readability and styling

--- src/features/Records/RecordsPreviewDrawer.tsx
+++ src/features/Records/RecordsPreviewDrawer.tsx
@@ -3,6 +3,7 @@
 import { BankMovement } from "@/shared/types/records.types";
 import { RiDeleteBinFill, RiPencilLine } from "@remixicon/react";
 import { Drawer, DrawerHeader, DrawerItems } from "flowbite-react";
+import clsx from "clsx"
 
 interface RecordsPreviewDrawerProps {
   record: BankMovement | null;
@@ -14,21 +15,32 @@ export const RecordsPreviewDrawer = ({ open, handleClose, record }: RecordsPrevi
   if (!record) {
     return null;
   }
+  const priceStyles = clsx(
+    "text-xl font-semibold text-center",
+    { "text-red-600": record.typeOfRecord === 'expense'},
+    { "text-green-400": record.typeOfRecord === 'income' },
+    { "text-blue-400": record.typeOfRecord === 'transfer' }
+  )
 
   return (
     <Drawer open={open} onClose={handleClose} position="right">
       <DrawerHeader title={`${record.fullDate} ${record.formattedTime}`} />
       <DrawerItems>
-        <h4>{record.shortName}</h4>
-        <div className="flex gap-3">
-          <button aria-label={`Edit record ${record.shortName}`}className="cursor-pointer mr-3">
-            <RiPencilLine />
-          </button>
-          <button aria-label={`Remove record ${record.shortName}`}className="cursor-pointer mr-3">
-            <RiDeleteBinFill />
-          </button>
+        <div className="flex flex-col gap-4">
+          <h4 className="text-2xl font-bold text-center">{record.shortName}</h4>
+          <div className="flex justify-center gap-3">
+            <button aria-label={`Edit record ${record.shortName}`}className="cursor-pointer mr-3">
+              <RiPencilLine size={18}  />
+            </button>
+            <button aria-label={`Remove record ${record.shortName}`}className="cursor-pointer mr-3">
+              <RiDeleteBinFill size={18} />
+            </button>
+          </div>
         </div>
-        <p>{record.amountFormatted}</p>
+
+        <div className="mt-10">
+          <p className={priceStyles}>{record.amountFormatted}</p>
+        </div>
       </DrawerItems>
     </Drawer>
   )
