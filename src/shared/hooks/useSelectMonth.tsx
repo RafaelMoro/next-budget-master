65f09ceb79c06de4737efb2988ff73d43fa035a8
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-07-16 01:02:13 -0600

    Refactor month selection logic in useSelectMonth hook and update SelectMonthDropdown to handle null selectedMonth

--- src/shared/hooks/useSelectMonth.tsx
+++ src/shared/hooks/useSelectMonth.tsx
@@ -1,12 +1,18 @@
-import { useState } from "react"
-import { CompleteMonthsType } from "../types/global.types"
+import { useEffect, useState } from "react"
+import { CompleteMonthsType, MONTHS } from "../types/global.types"
 
 /**
 * It's to select month for the dropdown SelectMonthDropdown
 */
 export const useSelectMonth = () => {
-  const [selectedMonth, setSelectedMonth] = useState<CompleteMonthsType>('Enero')
+  const [selectedMonth, setSelectedMonth] = useState<CompleteMonthsType | null>(null)
   const updateSelectMonth = (newMonth: CompleteMonthsType) => setSelectedMonth(newMonth)
+
+  useEffect(() => {
+    const monthNames: CompleteMonthsType[] = [...MONTHS]
+    const currentMonth = new Date().getMonth()
+    setSelectedMonth(monthNames[currentMonth])
+  }, [])
 
   return {
     selectedMonth,
--- src/shared/ui/atoms/SelectMonthDropdown.tsx
+++ src/shared/ui/atoms/SelectMonthDropdown.tsx
@@ -1,31 +1,28 @@
-import { useMemo, useState } from "react";
+import { useMemo } from "react";
 import { RiArrowDownSLine } from "@remixicon/react"
 import { Button, Dropdown, DropdownItem } from "flowbite-react"
 import { CompleteMonthsType, MONTHS } from "@/shared/types/global.types";
 
 interface SelectMonthDropdownProps {
-  selectedMonth: CompleteMonthsType
+  selectedMonth: CompleteMonthsType | null
   changeSelectedMonth: (newMonth: CompleteMonthsType) => void
 }
 
 export const SelectMonthDropdown = ({ selectedMonth, changeSelectedMonth }: SelectMonthDropdownProps) => {
   const allMonths: CompleteMonthsType[] = useMemo(() => [...MONTHS], []);
-  const [filteredMonths, setFilteredMonths] = useState<CompleteMonthsType[]>(allMonths)
   
   const handleSelectAccountType = (newMonth: CompleteMonthsType) => {
     changeSelectedMonth(newMonth)
-    const filteredMonths = allMonths.filter(month => month !== newMonth)
-    setFilteredMonths(filteredMonths)
   }
 
   return (
     <Dropdown label="" renderTrigger={() => (
-      <Button color="dark">
+      <Button color="light">
         Mes: {selectedMonth}
         <RiArrowDownSLine />
       </Button>
     )}>
-      { filteredMonths.map((month, index) => (
+      { allMonths.map((month, index) => (
         <DropdownItem
           onClick={() => handleSelectAccountType(month)}
           value={month}
