13792085ba1c16a6aff2bf61027159d9b4d085f7
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-07-25 00:20:56 -0600

    feat: add symmetricDifferenceExpensesRelated utility function for expense comparison

--- src/shared/utils/records.utils.ts
+++ src/shared/utils/records.utils.ts
@@ -166,3 +166,14 @@ export const getOriginAccountForEdit = ({
   }
   return '';
 };
+
+export const symmetricDifferenceExpensesRelated = (oldArray: ExpensePaid[], newArray: ExpensePaid[]) => {
+  const getId = (expense: ExpensePaid) => expense._id;
+  const firstDifference = oldArray.filter((oldExpense) => !newArray.some((newExpense) => getId(oldExpense) === getId(newExpense)));
+  const secondDifference = newArray.filter((newExpense) => !oldArray.some((oldExpense) => getId(newExpense) === getId(oldExpense)));
+
+  return {
+    oldRecords: firstDifference,
+    newRecords: secondDifference,
+  };
+}
