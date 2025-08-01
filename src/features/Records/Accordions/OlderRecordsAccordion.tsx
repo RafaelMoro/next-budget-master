67ea19808081bfe7938deda7883b2c489031dd7c
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-08-01 12:03:19 -0600

    feat: integrate DateRangeSearchPanel into OlderRecordsAccordion

--- src/features/Records/Accordions/OlderRecordsAccordion.tsx
+++ src/features/Records/Accordions/OlderRecordsAccordion.tsx
@@ -1,15 +1,26 @@
 "use client"
-import { useState } from "react"
+import { FormEvent, useState } from "react"
 import { Accordion, AccordionContent, AccordionPanel, AccordionTitle } from "flowbite-react"
 
 import { EmptyAccordionResult } from "./EmptyAccordionResult"
 import { useRecordPreview } from "@/shared/hooks/useRecordPreview"
 import { RecordsPreviewDrawer } from "../RecordsPreviewDrawer"
+import { DateRangeSearchPanel } from "@/shared/ui/molecules/DateRangeSearchPanel"
+import { useSelectMonth } from "@/shared/hooks/useSelectMonth"
+import { useSelectYear } from "@/shared/hooks/useSelectYear"
 
 export const OlderRecordsAccordion = () => {
   const [isOpen, setIsOpen] = useState(false)
   const handleAccordionTitleClick = () => {
     setIsOpen(true)
+  }
+
+  const { selectedMonth, updateSelectMonth, allMonths } = useSelectMonth()
+  const { selectedYear, updateSelectYear } = useSelectYear()
+  const handleSubmitGetRecord = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    event.stopPropagation()
+    // fetch records
   }
 
   const {
@@ -22,6 +33,14 @@ export const OlderRecordsAccordion = () => {
         <AccordionPanel>
           <AccordionTitle>Transacciones anteriores</AccordionTitle>
           <AccordionContent>
+            <DateRangeSearchPanel
+              allMonths={allMonths}
+              selectedMonth={selectedMonth}
+              selectedYear={selectedYear}
+              handleSubmit={handleSubmitGetRecord}
+              changeSelectedMonth={updateSelectMonth}
+              changeSelectedYear={updateSelectYear}
+            />
             <EmptyAccordionResult />
           </AccordionContent>
         </AccordionPanel>
