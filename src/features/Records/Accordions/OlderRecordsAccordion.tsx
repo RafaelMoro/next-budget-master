db9c76c83b659904a69f4165dee651ad62a2ddc0
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-08-01 23:05:22 -0600

    feat: enhance OlderRecordsAccordion to support fetching and displaying older records with loading skeletons

--- src/features/Records/Accordions/OlderRecordsAccordion.tsx
+++ src/features/Records/Accordions/OlderRecordsAccordion.tsx
@@ -1,6 +1,8 @@
 "use client"
-import { FormEvent, useState } from "react"
-import { Accordion, AccordionContent, AccordionPanel, AccordionTitle } from "flowbite-react"
+import { FormEvent, Fragment, useState } from "react"
+import { Accordion, AccordionContent, AccordionPanel, AccordionTitle, HR } from "flowbite-react"
+import { useQuery } from "@tanstack/react-query"
+import axios from "axios";
 
 import { EmptyAccordionResult } from "./EmptyAccordionResult"
 import { useRecordPreview } from "@/shared/hooks/useRecordPreview"
@@ -8,19 +10,38 @@ import { RecordsPreviewDrawer } from "../RecordsPreviewDrawer"
 import { DateRangeSearchPanel } from "@/shared/ui/molecules/DateRangeSearchPanel"
 import { useSelectMonth } from "@/shared/hooks/useSelectMonth"
 import { useSelectYear } from "@/shared/hooks/useSelectYear"
+import { GetRecordsResponse } from "@/shared/types/records.types";
+import { useDashboardStore } from "@/zustand/provider/dashboard-store-provider";
+import { OLDER_RECORDS_TAG } from "@/shared/constants/Global.constants";
+import { RecordEntrySkeleton } from "../RecordEntrySkeleton";
+import { RecordEntry } from "../RecordEntry";
 
 export const OlderRecordsAccordion = () => {
+  const selectedAccount = useDashboardStore(
+    (state) => state.selectedAccount
+  )
   const [isOpen, setIsOpen] = useState(false)
   const handleAccordionTitleClick = () => {
     setIsOpen(true)
   }
 
-  const { selectedMonth, updateSelectMonth, allMonths } = useSelectMonth()
-  const { selectedYear, updateSelectYear } = useSelectYear()
+  const { selectedMonth, updateSelectMonth, allMonths } = useSelectMonth({ isOlderRecords: true })
+  const { selectedYear, updateSelectYear } = useSelectYear({ isOlderRecords: true })
+
+  const { data, isPending, isSuccess, refetch } = useQuery({
+    queryKey: [OLDER_RECORDS_TAG, selectedAccount?._id],
+    queryFn: async () => {
+      const res: GetRecordsResponse = await axios.post('api/records', { accountId: selectedAccount?._id, month: selectedMonth, year: selectedYear })
+      return res?.data.data
+    },
+    enabled: isOpen,
+  })
+  const records = data?.records ?? []
+
   const handleSubmitGetRecord = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     event.stopPropagation()
-    // fetch records
+    refetch()
   }
 
   const {
@@ -33,15 +54,28 @@ export const OlderRecordsAccordion = () => {
         <AccordionPanel>
           <AccordionTitle>Transacciones anteriores</AccordionTitle>
           <AccordionContent>
-            <DateRangeSearchPanel
-              allMonths={allMonths}
-              selectedMonth={selectedMonth}
-              selectedYear={selectedYear}
-              handleSubmit={handleSubmitGetRecord}
-              changeSelectedMonth={updateSelectMonth}
-              changeSelectedYear={updateSelectYear}
-            />
-            <EmptyAccordionResult />
+            <div className="mb-5">
+                <DateRangeSearchPanel
+                  allMonths={allMonths}
+                  selectedMonth={selectedMonth}
+                  selectedYear={selectedYear}
+                  handleSubmit={handleSubmitGetRecord}
+                  changeSelectedMonth={updateSelectMonth}
+                  changeSelectedYear={updateSelectYear}
+                />
+              </div>
+              { isPending && Array.from({ length: 3 }).map((_, index) => (
+                <RecordEntrySkeleton key={index} />
+              ))}
+              { (records.length > 0 && isSuccess) && records.map((record, index) => (
+                <Fragment key={record._id}>
+                  <RecordEntry record={record} handleOpenRecordPreviewDrawer={handleOpenRecordPreviewDrawer} />
+                  {index !== (records.length - 1) && <HR />}
+                </Fragment>
+              ))}
+              { (records.length === 0 && isSuccess) && (
+                <EmptyAccordionResult />
+              )}
           </AccordionContent>
         </AccordionPanel>
       </Accordion>
