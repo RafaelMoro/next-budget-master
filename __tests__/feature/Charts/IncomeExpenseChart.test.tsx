76c344211c7c21be14e6d602523dafe6d4ed22d6
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-08-04 09:50:57 -0600

    Fix ResizeObserver error and add test for empty records in IncomeExpensesChart

--- __tests__/feature/Charts/IncomeExpenseChart.test.tsx
+++ __tests__/feature/Charts/IncomeExpenseChart.test.tsx
@@ -4,6 +4,7 @@ import { IncomeExpensesChart } from "@/features/Charts/IncomeExpensesChart"
 import { recordMock } from "../../mocks/records.mock"
 
 describe('IncomeExpenseChart', () => {
+  // Fix error: ResizeObserver is not defined
   window.ResizeObserver =
     window.ResizeObserver ||
     jest.fn().mockImplementation(() => ({
@@ -17,5 +18,12 @@ describe('IncomeExpenseChart', () => {
 
     expect(screen.getByText('Ingresos vs Gastos')).toBeInTheDocument()
     expect(screen.getByTestId('income-expenses-chart')).toBeInTheDocument()
+  })
+
+  it('Given no records, Show message of not enough data to show a chart', () => {
+    render(<IncomeExpensesChart records={[]} />)
+
+    expect(screen.getByText('Ingresos vs Gastos')).toBeInTheDocument()
+    expect(screen.getByText('No hay datos suficientes para mostrar el gráfico.')).toBeInTheDocument()
   })
 })
